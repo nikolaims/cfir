2ca8596a675aba2d67491310eb2b25b77bd2848a
nikolaims <n.m.smetanin@gmail.com>
2019-01-25 18:05:43 +0300

    eeg sim

--- data/sim_spectrum.py
+++ data/sim_spectrum.py
@@ -1,83 +1,159 @@
-import h5py
 import numpy as np
+import h5py
 import pylab as plt
 import scipy.signal as sg
-import pandas as pd
 import os
+from seaborn import color_palette
+import pickle
+
+ALPHA_BAND_EXT = (7, 13)
+ALPHA_BAND_HALFWIDTH = 2
+ALPHA_MAIN_FREQ = 10
+FS = 500
+WELCH_NPERSEG = FS
 
 
+def load_raw_p4_rest_probes():
+    # load 100 second probes (Baseline) for experiments in data_folder
+    data_folder = '/home/kolai/Data/alpha_delay2'
+    for exp_folder in map(lambda x: os.path.join(data_folder, x), os.listdir(data_folder)):
+        with h5py.File(os.path.join(exp_folder, 'experiment_data.h5')) as f:
+            if 'protocol4' not in f or len(f) < 20: continue
+            fs_ = int(f['fs'].value)
+            p4_index = list(map(bytes.decode, f['channels'][:])).index('P4')
+            x = f['protocol4/raw_data'][:][20 * FS:FS * 120, p4_index]
+            if len(x)<FS*70 or f['protocol4'].attrs['name']!= 'Baseline' or fs_!=FS: continue
+            print(f['protocol4'].attrs['name'], len(x) / FS, FS)
+            yield x
 
-data_folder = '/home/kolai/Data/alpha_delay2'
-pxxs = []
-alpha_hill = []
-for exp_folder in map(lambda x: os.path.join(data_folder, x), os.listdir(data_folder)):
+# collect data
+rests = np.array([x for x in load_raw_p4_rest_probes()])
 
-    with h5py.File(os.path.join(exp_folder, 'experiment_data.h5')) as f:
-        if 'protocol4' not in f or len(f) < 20: continue
-        fs = int(f['fs'].value)
-        p4_index = list(map(bytes.decode, f['channels'][:])).index('P4')
-        x = f['protocol4/raw_data'][:][:, p4_index]
+# estimate spectra
+freq = np.fft.rfftfreq(WELCH_NPERSEG, 1 / FS)
+alpha_mask = np.logical_and(freq > ALPHA_BAND_EXT[0], freq < ALPHA_BAND_EXT[1])
+background_spectra = []
+alpha_peaks = []
+for x in rests:
+    # welch boxcar window
+    pxx = sg.welch(x, FS, window=np.ones(WELCH_NPERSEG), scaling='spectrum')[1] ** 0.5
 
-        if len(x)<fs*70 or f['protocol4'].attrs['name']!='Baseline': continue
-        print(f['protocol4'].attrs['name'], len(x) / fs, fs)
-
-
-    n_samples = fs*1
-    x = x[30*fs:]
-    x = (x - x.mean())/x.std()
-    freq, pxx = sg.welch(x, fs, window=np.ones(n_samples), scaling='spectrum')
-    alpha_mask = np.logical_and(freq>8, freq<13)
+    # find individual alpha mask range
     main_freq = freq[alpha_mask][np.argmax(pxx[alpha_mask])]
-    alpha_mask = np.abs(freq - main_freq) <= 2
-    alpha_hill.append(pxx[alpha_mask]-np.interp(freq[alpha_mask], freq[~alpha_mask], pxx[~alpha_mask]))
-    ppx_no_alpha = np.interp(freq, freq[~alpha_mask], pxx[~alpha_mask])
+    ind_alpha_mask = np.abs(freq - main_freq) <= ALPHA_BAND_HALFWIDTH
 
-    plt.plot(main_freq, pxx[freq==main_freq], '.k')
+    # store alpha peak minus flanker interpolation
+    alpha_pxx = pxx[ind_alpha_mask] - np.interp(freq[ind_alpha_mask], freq[~ind_alpha_mask], pxx[~ind_alpha_mask])
+    alpha_peaks.append(alpha_pxx)
+
+    # alpha range flanker interpolation
+    ppx_no_alpha = np.interp(freq, freq[~ind_alpha_mask], pxx[~ind_alpha_mask])
+    background_spectra.append(ppx_no_alpha)
+
+    # viz spectra
+    plt.plot(main_freq, pxx[freq == main_freq], '.k')
     plt.plot(freq, pxx, 'b', alpha=0.1)
-    pxxs.append(ppx_no_alpha)
 
+# background spectrum
+background_spectrum = np.median(background_spectra, 0)
+background_spectrum[0] = 0
 
+# alpha spectrum
+alpha_spectrum = np.zeros_like(background_spectrum)
+alpha_mask = np.abs(freq - ALPHA_MAIN_FREQ) <= ALPHA_BAND_HALFWIDTH
+alpha_spectrum[alpha_mask] = np.median(alpha_peaks, 0)
 
-
-background_spec = np.exp(np.median(np.log(pxxs), 0))**0.5
-background_spec[0] = 0
-plt.plot(freq, background_spec, 'r')
+# viz background spectrum
+plt.plot(freq, background_spectrum, 'r')
+plt.plot(freq[alpha_mask], (background_spectrum+alpha_spectrum)[alpha_mask])
 plt.xlabel('Freq, Hz')
 plt.semilogy()
-plt.show()
-
-plt.figure()
-plt.plot(freq[alpha_mask], np.clip(alpha_hill, 1e-10, np.inf).T, 'b', alpha=0.1)
-plt.plot(freq[alpha_mask], np.exp(np.median(np.log(np.clip(alpha_hill, 1e-10, np.inf)), 0))*3, 'r')
-
+plt.ylim(1e-8, 1e-5)
+plt.xlim(0, FS/2)
 plt.show()
 
 
+# simulate background eeg
+n_seconds_to_sim = 120
+def sim_from_spec(n_seconds, freq, spectrum):
+    n_samples = FS * n_seconds + 1
 
-n_samples = fs*100 + 1
-freq_full = np.fft.fftfreq(n_samples, 1/fs)
-amplitudes = np.interp(np.abs(freq_full), freq, background_spec)
+    # frequencies
+    freq_full = np.fft.fftfreq(n_samples, 1 / FS)
 
-# random dft coefficients phases
-rand_phi = 1j*np.random.uniform(0, np.pi*2, n_samples//2)
-exponents = np.exp(np.concatenate([[0], rand_phi,  -rand_phi[::-1]]))
+    # dft coefficients amplitudes
+    amplitudes = np.interp(np.abs(freq_full), freq, spectrum)
 
-# inverse dft
-x_sim = np.real_if_close(np.fft.ifft(amplitudes*exponents)[:n_samples])
-band = [8, 12]
-t = np.arange(n_samples) / fs
-amp = sg.filtfilt(*sg.butter(6, (band[1] - band[0]) / fs/2, 'low'), np.random.normal(size=n_samples*2))[:n_samples]
-alpha = np.sin(2 * np.pi * t * sum(band) / 2) * amp
-x_sim += alpha/500
-x_sim = (x_sim - x_sim.mean())/x_sim.std()
-#plt.plot(x_sim)
-#plt.show()
-#
-freq_full, pxx_full = sg.welch(x_sim[:fs*120], fs, window=np.ones(fs), scaling='spectrum')
+    # random dft coefficients phases
+    rand_phi = 1j*np.random.uniform(0, np.pi*2, n_samples//2)
+    exponents = np.exp(np.concatenate([[0], rand_phi,  -rand_phi[::-1]]))
 
-plt.plot(freq_full, pxx_full)
-plt.plot(freq, (background_spec**2))
-plt.plot(freq, pxx)
+    # inverse dft
+    x_sim = np.real_if_close(np.fft.ifft(amplitudes*exponents)[:n_samples])
+    return x_sim
+
+# simulate background eeg
+background_sim = sim_from_spec(n_seconds_to_sim, freq, background_spectrum)
+
+# normalize background eeg sim
+_, pxx_full = sg.welch(background_sim, FS, window=np.ones(FS), scaling='spectrum')
+background_sim *= (background_spectrum[alpha_mask]).mean() / (pxx_full[alpha_mask] ** 0.5).mean()
+
+# simulate alpha sim
+alpha_sim = sim_from_spec(n_seconds_to_sim, freq, alpha_spectrum)
+
+# normalize alpha sim
+_, pxx_full = sg.welch(alpha_sim, FS, window=np.ones(FS), scaling='spectrum')
+alpha_sim *= (alpha_spectrum[alpha_mask]).mean()/(pxx_full[alpha_mask]**0.5).mean()
+
+# extract envelope
+alpha_sim_env = np.abs(sg.hilbert(alpha_sim))
+
+# pickle dump
+sim_dict = {'alpha': alpha_sim, 'envelope': alpha_sim_env, 'noise': background_sim}
+with open('alpha_sim_snr.pkl', 'wb') as handle:
+    pickle.dump(sim_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# crop edges
+crop_samples = 10*FS
+background_sim = background_sim[crop_samples:-crop_samples]
+alpha_sim = alpha_sim[crop_samples:-crop_samples]
+alpha_sim_env = alpha_sim_env[crop_samples:-crop_samples]
+
+# snr normalization
+noise_magnitude = np.mean(sg.welch(background_sim, FS, window=np.ones(FS), scaling='spectrum')[1][alpha_mask] ** 0.5)
+alpha_magnitude = np.mean(sg.welch(alpha_sim, FS, window=np.ones(FS), scaling='spectrum')[1][alpha_mask] ** 0.5)
+alpha_sim *= noise_magnitude/alpha_magnitude
+alpha_sim_env *= noise_magnitude/alpha_magnitude
+
+# viz snrs and specra
+snrs = np.concatenate([[0], np.logspace(0, 1, 5)])
+cm = color_palette('Reds_r', len(snrs))
+
+for color, snr in zip(cm, snrs):
+    sim_spectrum = sg.welch(background_sim + alpha_sim  * snr, FS, window=np.ones(FS), scaling='spectrum')[1] ** 0.5
+
+    plt.plot(freq, sim_spectrum, color=color, label='SNR = {:.2f}'.format(snr))
+
+plt.xlabel('Frequency, Hz')
+plt.ylabel('Magnitude, V')
+plt.xlim(0, 40)
+plt.ylim(1e-7, 1e-4)
+plt.plot(freq, background_spectrum, label='Median real\nP4 spectrum', alpha=0.6)
 plt.semilogy()
+plt.legend()
 plt.show()
 
+# viz ts
+fig, axes = plt.subplots(len(snrs), sharex=True)
+plt.subplots_adjust(hspace=0)
+t = np.arange(len(background_sim))/FS
+for color, snr, ax in zip(cm, snrs, axes):
+    ax.plot(t, background_sim + alpha_sim * snr)
+    ax.set_yticks([])
+    ax.set_ylabel('SNR = {:.2f}'.format(snr))
+axes[-1].plot(t, alpha_sim * snrs[-1] , alpha=0.6)
+axes[-1].plot(t, alpha_sim_env * snrs[-1] )
+plt.xlim(0, 10)
+plt.xlabel('Time, s')
+plt.show()
--- experiments/comp_tradeoff_snr.py
+++ experiments/comp_tradeoff_snr.py
@@ -1,56 +1,90 @@
 import numpy as np
 from pycfir.filters import get_x_chirp, RectEnvDetector, CFIRBandEnvelopeDetector, HilbertWindowFilter, rt_emulate, FiltFiltRectSWFilter
 import pylab as plt
+import pickle
+import pandas as pd
+import seaborn as sns
 
+
+def delay_align(x, y, delay):
+    if delay >= 0:
+        x = x[delay:]
+        y = y[:-delay or None]
+    else:
+        x = x[:delay]
+        y = y[abs(delay):]
+    return x, y
+
+
+def corr_delay(x, y, delay):
+    x, y = delay_align(x, y, delay)
+    corr = np.corrcoef(x, y)[0, 1]
+    return corr
+
+# load sim data
+with open('alpha_sim_snr.pkl', 'rb') as handle:
+    sim_dict = pickle.load(handle)
+
+# params
+n_seconds = 20
 fs = 500
+delays = np.arange(-50, 100, 5)
+snrs = [0, 1, 5]
+stats = pd.DataFrame(columns=['method', 'delay', 'corr', 'snr'])
 
-delays = np.arange(20, 300, 10)
-noises = np.linspace(0, 1, 10)
+# iterate cross snr-s
+for snr in snrs:
+    print(snr)
+    x = sim_dict['alpha'][:fs*n_seconds] * snr + sim_dict['noise'][:fs * n_seconds]
+    amp = sim_dict['envelope'][:fs*n_seconds]
 
-acorr1 = []
-acorr2 = []
-acorr3 = []
-
-for noise in noises:
-    print(noise)
-    x, amp = get_x_chirp(fs)
-    x += np.random.normal(size=len(x))*noise
-
-    # rect filter grid search
-    opt_corrs = np.zeros(len(delays))
     for j, delay in enumerate(delays):
-        corrs = []
-        for k in range(1, delay):
-            n_taps = [k*2, 2*delay-2*k]
-            filt = RectEnvDetector([8, 12], fs, k*2, 2*delay-2*k)
-            y = filt.apply(x)[sum(n_taps)//2:]
-            y_true = amp[:-sum(n_taps)//2]
-            corr = np.corrcoef(y, y_true)[0, 1]
-            corrs.append(0 if np.isnan(corr) else corr)
-        opt_corrs[j] = np.max(corrs)
+        # Rect. env. detector
+        if delay > 0:
+            corrs = []
+            for k in range(1, delay):
+                method = RectEnvDetector([8, 12], fs, k * 2, 2 * delay - 2 * k)
+                corr = corr_delay(method.apply(x), amp, delay)
+                corrs.append(0 if np.isnan(corr) else corr)
+            opt_corr = np.max(corrs)
+        else:
+            opt_corr = 0
+        stats = stats.append({'method': 'Rect.', 'delay': delay, 'corr': opt_corr, 'snr': snr}, ignore_index=True)
 
-    acorr1.append(np.mean([max(opt_corrs[:k]) for k in range(1, len(opt_corrs))]))
-    # cfir filter
-    opt_corrs = np.zeros(len(delays))
-    for j, delay in enumerate(delays):
-        filt = CFIRBandEnvelopeDetector([8, 12], fs, delay, n_taps=1000, n_fft=2000)
-        y = filt.apply(x)[delay:]
-        y_true = amp[:-delay]
-        corr = np.corrcoef(y, y_true)[0, 1]
-        opt_corrs[j] = corr
-    acorr2.append(np.mean([max(opt_corrs[:k]) for k in range(1, len(opt_corrs))]))
+        # cFIR env detector
+        method = CFIRBandEnvelopeDetector([8, 12], fs, delay, n_taps=1000, n_fft=2000)
+        opt_corr = corr_delay(method.apply(x), amp, delay)
+        stats = stats.append({'method': 'cFIR', 'delay': delay, 'corr': opt_corr, 'snr': snr}, ignore_index=True)
 
-    # windowed hilbert
-    opt_corrs = np.zeros(len(delays))
-    for j, delay in enumerate(delays):
-        filt = HilbertWindowFilter(500, fs, [8, 12], delay)
-        y = np.abs(rt_emulate(filt, x)[delay:])
-        y_true = amp[:-delay]
-        corr = np.corrcoef(y, y_true)[0, 1]
-        opt_corrs[j] = corr
-    acorr3.append(np.mean([max(opt_corrs[:k]) for k in range(1, len(opt_corrs))]))
+        # Hilbert
+        if delay >= 0:
+            method = HilbertWindowFilter(500, fs, [8, 12], delay)
+            opt_corr = corr_delay(np.abs(rt_emulate(method, x, 10)), amp, delay)
+        else:
+            opt_corr = 0
+        stats = stats.append({'method': 'wHilbert', 'delay': delay, 'corr': opt_corr, 'snr': snr}, ignore_index=True)
 
-plt.plot(noises, acorr1)
-plt.plot(noises, acorr2)
-plt.plot(noises, acorr3)
+
+
+# viz methods
+methods_linestyles = ['-.', '-', '--']
+methods = ['Rect.', 'cFIR', 'wHilbert']
+#colors = ["windows blue", "amber", "greyish", "faded green", "dusty purple"]
+snrs_colors = sns.xkcd_palette(["red", 'amber', 'medium green', 'blue'])
+
+for method, method_linestyle in zip(methods, methods_linestyles):
+    plt.plot(np.nan, color='k', linestyle=method_linestyle, label=method)
+for snr, snr_color in zip(snrs, snrs_colors):
+    plt.plot(np.nan,  color=snr_color, label='SNR={}'.format(snr))
+plt.legend()
+
+
+for method, method_linestyle in zip(methods, methods_linestyles):
+    for snr, snr_color in zip(snrs, snrs_colors):
+        data = stats.query('method=="{}" & snr=={}'.format(method, snr))
+        plt.plot(data['delay'].values/fs*1000, data['corr'].values, color=snr_color, linestyle=method_linestyle, label=None)
+
+plt.axvline(0, color='k', alpha=0.3)
+plt.xlabel('Delay, ms')
+plt.ylabel('Corr.')
 plt.show()

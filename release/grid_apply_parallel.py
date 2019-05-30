084e7643624f0fc7c1c9f6da078f93d1c396d57f
nikolaims <n.m.smetanin@gmail.com>
2019-05-30 17:10:31 +0300

    grid apply refactor

--- release/grid_apply_parallel.py
+++ release/grid_apply_parallel.py
@@ -1,15 +1,27 @@
-from release.filters import RectEnvDetector, CFIRBandEnvelopeDetector, AdaptiveCFIRBandEnvelopeDetector
-import numpy as np
-from release.constants import DELAY_RANGE, FS, N_SAMPLES_TRAIN, N_SAMPLES_TEST
 import itertools
+import multiprocessing
+import numpy as np
 import pandas as pd
+
 from tqdm import tqdm
 from copy import deepcopy
-import time
-import multiprocessing
 
+from release.filters import RectEnvDetector, CFIRBandEnvelopeDetector, AdaptiveCFIRBandEnvelopeDetector
+from release.constants import DELAY_RANGE, FS, N_SAMPLES_TRAIN, N_SAMPLES_TEST
 from release.utils import magnitude_spectrum
 
+
+# load data
+eeg_df = pd.read_pickle('data/rest_state_probes_real.pkl')
+
+# compute train magnitude spectrum for weights if it needed
+magnitude_spectrum_train = {}
+for dataset, dataset_df in eeg_df.groupby('dataset'):
+    _, weights = magnitude_spectrum(eeg_df['eeg'].values[:N_SAMPLES_TRAIN], FS)
+    magnitude_spectrum_train[dataset] = weights
+
+
+# init grid search space
 kwargs_grid_dict = {}
 
 # rls
@@ -17,9 +29,8 @@ kwargs_grid_dict['rlscfir'] = (AdaptiveCFIRBandEnvelopeDetector, {
     'delay': DELAY_RANGE,
     'n_taps': [250, 500, 1000],#np.arange(200, 2000 + 1, 300),
     'ada_n_taps': [5000],
-    'mu': [0.9, 0.8, 0.7],
+    'mu': [0.7],
 })
-
 
 # cFIR
 kwargs_grid_dict['cfir'] = (CFIRBandEnvelopeDetector, {
@@ -42,53 +53,58 @@ kwargs_grid_dict['rect'] = (RectEnvDetector, {
 })
 
 
-eeg_df = pd.read_pickle('data/rest_state_probes_real.pkl')#.query('dataset=="alpha2-delay-subj-1_11-06_17-15-29"')
-
-magnitude_spectrum_train = {}
-for dataset, dataset_df in eeg_df.groupby('dataset'):
-    _, weights = magnitude_spectrum(eeg_df['eeg'].values[:N_SAMPLES_TRAIN], FS)
-    magnitude_spectrum_train[dataset] = weights
-
-
+# loop over methods
 for method_name in kwargs_grid_dict:
-
-    start = time.time()
+    # unpack class and grid params
     method_class, kwargs_grid = kwargs_grid_dict[method_name]
 
+    # add datasets to params
     kwargs_grid['dataset'] = eeg_df['dataset'].unique()
+
+    # all combinations of params to list
     keys, values = zip(*kwargs_grid.items())
     kwargs_list = list(map(lambda x: dict(zip(keys, x)), itertools.product(*values)))
+
+    # tqdm status monitor settings
     pbar = tqdm(total=len(kwargs_list)//4, desc=method_name)
 
+    # method to parallel computing
     def grid_apply_method(method_kwargs):
-        global method_class
+        # init results array
         res = np.empty(N_SAMPLES_TRAIN + N_SAMPLES_TEST, dtype='complex64')
+        # method params
         method_kwargs = deepcopy(method_kwargs)
+
+        # get x and train band
         df = eeg_df.query('dataset=="{}"'.format(method_kwargs['dataset']))
         x = df['eeg'].values
         band = (df['band_left_train'].values[0], df['band_right_train'].values[0])
+
+        # handle weights logic
         if 'weights' in method_kwargs:
             if method_kwargs['weights'] is not None:
                 method_kwargs['weights'] = magnitude_spectrum_train[method_kwargs['dataset']]
-        try:
-            method = method_class(band=band, fs=FS, **method_kwargs)
-            y = method.apply(x[:N_SAMPLES_TRAIN])
-            res[:N_SAMPLES_TRAIN] = y
 
-            method = method_class(band=band, fs=FS, **method_kwargs)
-            y = method.apply(x[N_SAMPLES_TRAIN:N_SAMPLES_TRAIN+N_SAMPLES_TEST])
-            res[N_SAMPLES_TRAIN:N_SAMPLES_TRAIN+N_SAMPLES_TEST] = y
-        except ValueError:
-            res[:] = np.nan
+        # train data
+        method = method_class(band=band, fs=FS, **method_kwargs)
+        x_slice = x[:N_SAMPLES_TRAIN]
+        res[:N_SAMPLES_TRAIN] = method.apply(x_slice)
+
+        # test data
+        method = method_class(band=band, fs=FS, **method_kwargs)
+        x_slice = x[N_SAMPLES_TRAIN:N_SAMPLES_TRAIN+N_SAMPLES_TEST]
+        res[N_SAMPLES_TRAIN:N_SAMPLES_TRAIN+N_SAMPLES_TEST] = method.apply(x_slice)
+
+        # update tqdm bar
         pbar.update(1)
         return res, method_kwargs
 
-
+    # run grid search
     pool = multiprocessing.Pool(4)
     res, kwargs_df = zip(*pool.map(grid_apply_method, kwargs_list))
 
-
+    # save estimations
     np.save('results/{}.npy'.format(method_name), res)
-    pd.DataFrame(list(kwargs_df)).to_csv('results/{}_kwargs.csv'.format(method_name), index=False)
 
-    print(method_name, time.time() - start)
+    # save list of all params combinations
+    pd.DataFrame(list(kwargs_df)).to_csv('results/{}_kwargs.csv'.format(method_name), index=False)

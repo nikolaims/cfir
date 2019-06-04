0f1d4fedf823bb11d186e3e6469ff53553ff1fc3
nikolaims <n.m.smetanin@gmail.com>
2019-06-04 15:28:48 +0300

    5 percentile

--- release/results_auc.py
+++ release/results_auc.py
@@ -4,7 +4,7 @@ import numpy as np
 import seaborn as sns
 from release.filters import CFIRBandEnvelopeDetector, RectEnvDetector
 from release.utils import magnitude_spectrum
-from settings import FS, DELAY_RANGE
+from release.constants  import FS, DELAY_RANGE
 from sklearn.metrics import roc_auc_score, average_precision_score, balanced_accuracy_score
 
 
@@ -26,12 +26,12 @@ band = eeg_df[['band_left', 'band_right']].values[0]
 magnitude_spectrum_train = {}
 _, weights = magnitude_spectrum(eeg_df['eeg'].values, FS)
 
-stats_df = pd.read_pickle('results/stats.pkl').query('dataset=="{}" & delay=={} '.format(dataset, 0))
+stats_df = pd.read_pickle('results/stats.pkl').query('dataset=="{}"'.format(dataset))
 flatui = {'cfir':'#0099d8', 'acfir': '#84BCDA', 'wcfir':'#FE4A49', 'rect':'#A2A79E'}
 
 
-alpha=10
-DELAY_RANGE = np.linspace(-50, 100, 51, dtype=int)
+alpha=5
+#DELAY_RANGE = np.linspace(-50, 100, 51, dtype=int)
 acc = np.zeros(len(DELAY_RANGE))
 acc_rand = np.zeros(len(DELAY_RANGE))
 
@@ -45,8 +45,10 @@ for j_n_states, n_states in enumerate([2, 3]):
     for method_name, method_class in zip(
             ['cfir', 'rect', 'wcfir'],
             [CFIRBandEnvelopeDetector, RectEnvDetector, CFIRBandEnvelopeDetector]):
+        acc = np.zeros(len(DELAY_RANGE))*np.nan
         for d, DELAY in enumerate(DELAY_RANGE):
-            params = stats_df.query('method=="{}" & metric=="corr"'.format(method_name))['params'].values[0]
+            if method_name == 'rect' and DELAY <0: continue
+            params = stats_df.query('method=="{}" & metric=="corr" & delay=="{}"'.format(method_name, DELAY))['params'].values[0]
 
             params['weights'] = weights if method_name == 'wcfir' else None
             env_det = method_class(band=band, fs=FS, delay=DELAY, **params)
@@ -68,9 +70,9 @@ for j_n_states, n_states in enumerate([2, 3]):
             y_pred = get_classes(envelope_pred, alpha, n_states)
             acc[d] = balanced_accuracy_score(y_true, y_pred) if (method_name in ['cfir', 'wcfir'] or DELAY>=0) else np.nan
 
-        axes[j_n_states].plot(DELAY_RANGE*2, acc*100, label=method_name, color=flatui[method_name])
+        axes[j_n_states].plot(DELAY_RANGE*2, acc*100, '.-', label=method_name, color=flatui[method_name])
 
-    axes[j_n_states].plot(DELAY_RANGE*2, DELAY_RANGE*0 + balanced_accuracy_score(y_true, y_true*0)*100, color='k', label='all-high')
+    axes[j_n_states].plot(DELAY_RANGE*2, DELAY_RANGE*0 + balanced_accuracy_score(y_true, y_true*0)*100, '.-',  color='k', label='all-high')
 [ax.set_xlabel('Delay, ms') for ax in axes]
 axes[1].set_xlabel('Delay, ms')
 axes[1].legend()
@@ -83,4 +85,4 @@ axes[1].set_title('B. High/Middle/Low')
 #
 # sns.kdeplot(envelope, envelope0ms)
 
-plt.savefig('results/viz/res-classif.png', dpi=500)
+plt.savefig('results/viz/res-classification.png', dpi=500)
---
+++ release/results_pred_curve.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import pylab as plt
+import numpy as np
+import seaborn as sns
+from release.filters import CFIRBandEnvelopeDetector, RectEnvDetector
+from release.utils import magnitude_spectrum
+from release.constants import FS, DELAY_RANGE
+from sklearn.metrics import roc_auc_score, average_precision_score, balanced_accuracy_score
+
+
+def get_classes(y, alpha, n_states=3):
+    y_pred = np.zeros(len(y))
+    if n_states == 3:
+        y_pred[y > np.percentile(y, alpha)] = 1
+        y_pred[y > np.percentile(y, 100 - alpha)] = 2
+    if n_states == 2:
+        y_pred[y > np.percentile(y, 100 - alpha)] = 1
+    return y_pred
+
+dataset = "alpha2-delay-subj-21_12-06_12-15-09"
+eeg_df = pd.read_pickle('data/rest_state_probes_real.pkl').query('dataset=="{}"'.format(dataset))
+
+envelope = eeg_df['an_signal'].abs().values * 1e6
+band = eeg_df[['band_left', 'band_right']].values[0]
+
+magnitude_spectrum_train = {}
+_, weights = magnitude_spectrum(eeg_df['eeg'].values*1e6, FS)
+
+stats_df = pd.read_pickle('results/stats.pkl').query('dataset=="{}"'.format(dataset))
+flatui = {'cfir':'#0099d8', 'acfir': '#84BCDA', 'wcfir':'#FE4A49', 'rect':'#A2A79E'}
+
+
+#DELAY_RANGE = np.array([-100, -25, 0, 25, 100])
+acc = np.zeros(len(DELAY_RANGE))
+acc_rand = np.zeros(len(DELAY_RANGE))
+
+fig, axes = plt.subplots(2, 3, sharey=True, sharex=True, figsize=(7,3))
+plt.subplots_adjust(bottom=0.17, left=0.1, wspace=0, hspace=0)
+
+
+
+pre = 375
+post = 250
+t = np.arange(-pre, post)/FS*1000
+
+
+delay_dict = {-100: -50, -50: -50, -25:-25,   0:0,  25:25,  50:50, 100:50}
+lines = []
+for method_name, method_class in zip(
+        ['cfir', 'rect', 'wcfir'][1:3],
+        [CFIRBandEnvelopeDetector, RectEnvDetector, CFIRBandEnvelopeDetector][1:3]):
+    for d, DELAY in enumerate(DELAY_RANGE[::-1]):
+        if method_name == 'rect' and DELAY < 0: continue
+        params = stats_df.query('method=="{}" & metric=="corr" & delay=="{}"'.format(method_name, delay_dict[DELAY]))['params'].values[0]
+
+        params['weights'] = weights if method_name == 'wcfir' else None
+        env_det = method_class(band=band, fs=FS, delay=DELAY, **params)
+        envelope_pred = np.abs(env_det.apply(eeg_df['eeg'].values*1e6))
+        detections = np.where(np.diff(1*(envelope_pred>np.percentile(envelope_pred, 95)))>0)[0]
+        spindles = []
+        for detection in detections:
+            if detection > pre and detection < len(envelope_pred) - post:
+                spindles.append(envelope[detection-pre:detection+post])
+        h, = axes[d//3, d%3].plot(t, np.mean(spindles, 0), color=flatui[method_name])
+        # axes[d // 3, d % 3].plot(t, np.array(spindles).T, color=flatui[method_name], alpha=0.5)
+        axes[d // 3, d % 3].fill_between(t, np.mean(spindles, 0)-np.std(spindles, 0), np.mean(spindles, 0)+np.std(spindles, 0), color=flatui[method_name], alpha=0.1)
+        axes[d // 3, d % 3].text(-740, 12, 'D = {}ms'.format(DELAY*2))
+        if d==0: lines.append(h)
+
+spindles = []
+for detection in np.random.randint(pre, len(envelope)-post, 1000):
+    spindles.append(envelope[detection-pre:detection+post])
+h, = axes[1, 2].plot(t, np.mean(spindles, 0), color='k')
+lines.append(h)
+# axes[d // 3, d % 3].plot(t, np.array(spindles).T, color=flatui[method_name], alpha=0.5)
+axes[1, 2].fill_between(t, np.mean(spindles, 0) - np.std(spindles, 0),
+                                 np.mean(spindles, 0) + np.std(spindles, 0), color='k', alpha=0.1)
+axes[1, 2].text(-740, 12, 'rand')
+[ax.axvline(0, linestyle='--', color='k', alpha=0.5, zorder=-100) for ax in axes.ravel()]
+
+
+
+axes[1,1].set_xticks([-500, 0, 500])
+fig.text(0.02, 0.5, 'Envelope, $\mu V$', va='center', rotation='vertical')
+fig.text(0.5, 0.04, 'Time, s', ha='center')
+
+plt.figlegend(lines, ['rect', 'wcfir', 'rand'])
+plt.savefig('results/viz/res-spindles.png', dpi=500)

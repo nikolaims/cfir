6af9cd74007f45d5311d26bd1ffee8edd2a03b5c
nikolaims <n.m.smetanin@gmail.com>
2019-07-18 17:06:49 +0300

    env stats refactoring

--- release/envelope_stats.py
+++ release/envelope_stats.py
@@ -6,6 +6,7 @@ import pylab as plt
 import seaborn as sns
 
 
+# method to align offline and real-time signals
 def delay_align(x, y, delay):
     if delay >= 0:
         x = x[delay:]
@@ -15,88 +16,112 @@ def delay_align(x, y, delay):
         y = y[abs(delay):]
     return x, y
 
-
+# compute metrics: env correlation, phase0 bias, phase0 var
 def corr_delay(x, y, delay, bias=0):
     x, y = delay_align(x, y, delay)
     corr = np.corrcoef(np.abs(x), np.abs(y))[0, 1]
-    #phase = np.abs(np.mean(x/np.abs(x)*np.conj(y)/np.abs(y)))
     phase_zero_moments = np.diff((np.angle(x) >= 0 - bias/360*2*np.pi).astype(int))>0
     phase_bias = np.angle(y)[1:][phase_zero_moments].mean() / 2 / np.pi * 360
     phase_disp = (((np.angle(y)[1:][phase_zero_moments] / 2 / np.pi * 360 - phase_bias)**2).mean())**0.5
     return corr, phase_bias, phase_disp
 
 
-
+# load eeg
 eeg_df = pd.read_pickle('data/rest_state_probes_real.pkl')
 
+# utils
 methods = ['cfir', 'rlscfir', 'wcfir', 'rect', 'ffiltar']
+colors = ['#0099d8', '#84BCDA', '#FE4A49', '#A2A79E', '#444444']
+metrics = ['corr', 'phase_bias', 'phase_disp']
+slices = dict([('train', slice(None, N_SAMPLES_TRAIN)), ('test', slice(N_SAMPLES_TRAIN, N_SAMPLES_TRAIN + N_SAMPLES_TEST))])
+columns=['method', 'dataset', 'snr', 'sim', 'delay', 'metric', 'train', 'test', 'params']
 
-
-columns = ['method', 'dataset', 'snr', 'sim', 'delay', 'metric', 'train', 'test', 'params']
-dtypes = ['str', 'str', 'float', 'int', 'int', 'str', 'float', 'float', 'object']
-
+# best stats df
 stats_df = pd.DataFrame(columns=columns, dtype=float)
 
-slices = dict([('train', slice(None, N_SAMPLES_TRAIN)), ('test', slice(N_SAMPLES_TRAIN, N_SAMPLES_TRAIN + N_SAMPLES_TEST))])
 
-
-
+# iterate over methods
 for j_method, method_name in enumerate(methods):
+
+    # load grid results and params
     res = np.load('results/{}.npy'.format(method_name))
     kwargs_df = pd.read_csv('results/{}_kwargs.csv'.format(method_name))
 
+    # iterate datasets
     for dataset in tqdm(eeg_df['dataset'].unique(), method_name):
+
+        # query dataset data
         df = eeg_df.query('dataset == "{}"'.format(dataset))
+
+        # get snr, sim flag and y_true
         snr = df['snr'].values[0]
         sim = df['sim'].values[0]
         y_true = df['an_signal'].values
 
+        # iterate delays
         for delay in DELAY_RANGE:
             best_corr_dict = (None, None, None)
-            best_phase_bias_dict = (None, None, None)
-            best_phase_disp_dict = (None, None, None)
+            best_bias_dict = (None, None, None)
+            best_var_dict = (None, None, None)
+
+            # iterate params grid
             for params in kwargs_df.query('dataset == "{}" & delay=={}'.format(dataset, delay)).itertuples():
                 params = params._asdict()
                 y_pred = res[params.pop('Index')]
                 assert params.pop('dataset') == dataset
                 assert params.pop('delay') == delay
-                # if method_name == 'ffiltar': y_pred = np.roll(y_pred, 5)
-                train_corr, train_phase_bias, train_phase_disp = corr_delay(y_pred[slices['train']], y_true[slices['train']], delay)
-                if (train_corr > (best_corr_dict[0] or 0)) or (train_phase_bias < (best_phase_bias_dict[0] or 1000)) or (train_phase_disp < (best_phase_disp_dict[0] or 1000)):
-                    test_corr, test_phase_bias, test_phase_disp = corr_delay(y_pred[slices['test']], y_true[slices['test']], delay, train_phase_bias)
+
+                # train scores
+                train_corr, train_bias, train_var = corr_delay(y_pred[slices['train']], y_true[slices['train']], delay)
+
+                # if one of scores is better continue
+                if ((train_corr > (best_corr_dict[0] or 0)) or
+                    (abs(train_bias) < abs(best_bias_dict[0] or 1000)) or
+                    (train_var < (best_var_dict[0] or 1000))):
+
+                    # compute test scores
+                    test_corr, test_bias, test_var = corr_delay(y_pred[slices['test']], y_true[slices['test']],
+                                                                delay, train_bias)
+
+                    # if train env CORR is better save train test and params
                     if train_corr > (best_corr_dict[0] or 0):
                         best_corr_dict = (train_corr, test_corr, params)
-                    if train_phase_bias < (best_phase_bias_dict[0] or 1000):
-                        best_phase_bias_dict = (train_phase_bias, test_phase_bias, params)
 
-                    if train_phase_bias < (best_phase_disp_dict[0] or 1000):
-                        best_phase_disp_dict = (train_phase_disp, test_phase_disp, params)
+                    # if train BIAS is better save train test and params
+                    if abs(train_bias) < abs(best_bias_dict[0] or 1000):
+                        best_bias_dict = (train_bias, test_bias, params)
 
+                    # if train VAR is better save train test and params
+                    if train_var < (best_var_dict[0] or 1000):
+                        best_var_dict = (train_var, test_var, params)
 
+            # save stats
             stats_dict = {'method': method_name, 'dataset': dataset, 'snr': snr, 'sim': sim, 'delay': delay*2,
-                          'metric': ['corr', 'phase_bias', 'phase_disp'], 'train': [best_corr_dict[0], best_phase_bias_dict[0], best_phase_disp_dict[0]],
-                          'test': [best_corr_dict[1], best_phase_bias_dict[1], best_phase_disp_dict[1]], 'params': [best_corr_dict[2], best_phase_bias_dict[2], best_phase_disp_dict[2]]}
-
+                          'metric': metrics,
+                          'train':  [best_corr_dict[0], best_bias_dict[0], best_var_dict[0]],
+                          'test':   [best_corr_dict[1], best_bias_dict[1], best_var_dict[1]],
+                          'params': [best_corr_dict[2], best_bias_dict[2], best_var_dict[2]]}
             stats_df = stats_df.append(pd.DataFrame(stats_dict), ignore_index=True)
 
+# update dtypes
 stats_df['train'] = stats_df['train'].astype(float)
 stats_df['test'] = stats_df['test'].astype(float)
+stats_df['delay'] = stats_df['delay'].astype(int)
+
+# save stats df
 stats_df.to_pickle('results/stats.pkl')
 
-# stats_df = pd.read_pickle('results/stats.pkl')
-stats_df['snr_cat'] = stats_df['snr'].apply(lambda x: 'High' if x> stats_df['snr'].median() else 'Low')
 
-stats_df['delay_cat'] = stats_df['delay'].astype(int)#.apply(lambda x: '[100, 200)' if x>=50 else ('[0, 100)' if x>=0 else '[-100, 0)'))
-
-
-#flatui = ["#F15152", "#11A09E", "#91BFBE"]
-flatui = ['#0099d8', '#84BCDA', '#FE4A49', '#A2A79E', '#444444',]
-#sns.set(rc={'figure.figsize': (5,5)})
-g = sns.catplot('delay_cat', 'test', 'method', data=stats_df, col='metric', sharey='col', kind='point', dodge=True, palette=sns.color_palette(flatui), linewidth=0, edgecolor='#CCCCCC', height=4, aspect=1)
+# plot metrics trade-off
+g = sns.catplot('delay', 'test', 'method', data=stats_df, col='metric', sharey='col', kind='point', dodge=0.2,
+                palette=sns.color_palette(colors), linewidth=0, edgecolor='#CCCCCC', height=4, aspect=1)
 def setup_axes(g, xlabel='Delay, ms'):
     [ax.axvline(2, color='k', alpha=0.5, linestyle='--', zorder=-1000) for ax in g.axes.flatten()]
+    g.axes[0, 1].axhline(0, color='k', alpha=0.5, linestyle=':', zorder=-1000)
     plt.subplots_adjust(wspace=0.35)
-    g.axes[0, 1].set_ylim(-5, 10)
+    g.axes[0, 0].set_ylim(0, 1)
+    g.axes[0, 1].set_ylim(-6, 10)
+    g.axes[0, 2].set_ylim(20, 100)
     g.axes[0,0].set_ylabel('$r_a$')
     g.axes[0,1].set_ylabel('$b_\phi$')
     g.axes[0,2].set_ylabel('$\sigma_\phi$')
@@ -108,24 +133,15 @@ def setup_axes(g, xlabel='Delay, ms'):
     g.axes[0,0].set_title('A. Envelope corr.')
     g.axes[0,1].set_title('B. Phase bias ')
     g.axes[0,2].set_title('C. Phase var.')
-
 setup_axes(g)
-#for j in range(2): [g.axes[1,j].axhline(k*7.2, color='k', alpha=0.1, linewidth=1, zorder=-100) for k in range(10)]
-
 plt.savefig('results/viz/res-metrics.png', dpi=500)
 
-
-
-g = sns.lmplot('snr', 'test', hue='method', data=stats_df.query('delay==0'), col='metric', sharey='none', palette=sns.color_palette(flatui), height=4, aspect=1, ci=None)
-
+# plot zero delay metrics vs SNR
+g = sns.lmplot('snr', 'test', hue='method', data=stats_df.query('delay==0'), col='metric', sharey='none',
+               palette=sns.color_palette(colors), height=4, aspect=1, ci=None)
 g.axes[0,0].set_xlim(stats_df.snr.min()-0.1, stats_df.snr.max()+0.1)
-g.axes[0,0].set_ylim(0, 1)
-g.axes[0,1].set_ylim(-10, 10)
-g.axes[0,2].set_ylim(30, 90)
+
 g.axes[0,1].lines[methods.index('rect')].set_alpha(0)
 g.axes[0,2].lines[methods.index('rect')].set_alpha(0)
 setup_axes(g, 'SNR')
-g.axes[0,1].set_ylim(-10, 10)
-g.axes[0, 1].set_yticklabels(['${:n}^\circ$'.format(x) for x in g.axes[0, 1].get_yticks()])
-
 plt.savefig('results/viz/res-metrics-delay0.png', dpi=500)

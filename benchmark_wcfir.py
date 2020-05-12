e092daa9e2e676bf4daa8cd09ca7e13abe16522e
nikolaims <n.m.smetanin@gmail.com>
2020-05-12 17:23:27 +0300

    prepare multichannel data

---
+++ benchmark_wcfir.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+from filters import CFIRBandEnvelopeDetector
+from constants import FS, N_SAMPLES_TRAIN, N_SAMPLES_TEST
+from utils import magnitude_spectrum, individual_max_snr_band, delay_align
+
+
+# constants
+DELAY = 0
+TRAIN_SLICE = slice(0, N_SAMPLES_TRAIN)
+TEST_SLICE = slice(N_SAMPLES_TRAIN, N_SAMPLES_TRAIN + N_SAMPLES_TEST)
+
+
+# performance metric
+def envelope_corr(pred, target):
+    pred, target = delay_align(pred, target, DELAY)
+    corr = np.corrcoef(np.abs(pred), np.abs(target))[1, 0]
+    return corr
+
+
+# load data
+eeg_df = pd.read_pickle('data/train_test_data_multichannel.pkl')
+# eeg_df  = eeg_df.query('subj_id==4')
+datasets = eeg_df.subj_id.unique()
+
+# loop over datasets
+res = np.empty((len(datasets), N_SAMPLES_TRAIN + N_SAMPLES_TEST), dtype='complex64')
+test_scores = np.zeros(len(datasets))
+for j_dataset, dataset in enumerate(datasets):
+
+    # load input signal 'x' and target signal 'target' for specific dataset (subject)
+    df = eeg_df.query('subj_id=={}'.format(dataset))
+    x = df['P4'].values
+    target = df['an_signal'].values
+
+    # train data: define 'train_band', 'train_weights' and 'train_n_taps' parameters on train data
+    x_train = x[TRAIN_SLICE]
+    train_band, _snr_train = individual_max_snr_band(x_train, FS)
+    _freqs, train_weights = magnitude_spectrum(x_train, FS)
+    best_train_score = 0
+    train_n_taps = None
+    for n_taps in [250, 500, 750, 1000]:
+        method = CFIRBandEnvelopeDetector(band=train_band, fs=FS, delay=DELAY, n_taps=n_taps, weights=train_weights)
+        res[j_dataset, TRAIN_SLICE] = method.apply(x_train)
+        train_score = envelope_corr(res[j_dataset, TRAIN_SLICE], target[TRAIN_SLICE])
+        if train_score > best_train_score:
+            train_n_taps = n_taps
+            best_train_score = train_score
+
+    # test data: estimate performance metric on test data
+    method = CFIRBandEnvelopeDetector(band=train_band, fs=FS, delay=DELAY, n_taps=train_n_taps, weights=train_weights)
+    x_test = x[TEST_SLICE]
+    res[j_dataset, TEST_SLICE] = method.apply(x_test)
+    test_scores[j_dataset] = envelope_corr(res[j_dataset, TEST_SLICE], target[TEST_SLICE])
+
+# print mean and std of test scores
+print('test score: {:.4f} +- {:.4f}'.format(test_scores.mean(), test_scores.std()))
--- prepare_data.py
+++ prepare_data.py
@@ -9,50 +9,56 @@ from constants import FS, N_SAMPLES_TEST, N_SAMPLES_TRAIN
 
 
 # collect info
-rest_df = pd.read_pickle('data/rest_10subjs_P4_500Hz_ICA_eye_rej.pkl')
+rest_df = pd.read_pickle('data/rest_10subjs_32ch_500Hz_ICA_eye_rej.pkl')
+channels = list(rest_df.columns[:-1])
 
-eeg_df = pd.DataFrame(columns=['subj_id', 'snr', 'band_left_train', 'band_right_train', 'band_left', 'band_right', 'eeg',
-                               'an_signal'])
+eeg_df = pd.DataFrame(columns=channels + ['subj_id', 'snr', 'band_left_train', 'band_right_train',
+                                                 'band_left', 'band_right', 'eeg', 'an_signal'])
+
 eeg_df['an_signal'] = eeg_df['an_signal'].astype('complex')
 
 # store data
 for j_dataset, dataset in enumerate(rest_df['subj_id'].unique()):
-    x = rest_df.loc[rest_df['subj_id']==j_dataset, 'P4']
 
-
-    # drop noisy datasets
-    if len(x) < N_SAMPLES_TRAIN+N_SAMPLES_TEST+10 * FS: continue
-
-
+    x = rest_df.loc[rest_df['subj_id'] == j_dataset, channels]
+    p4 = x['P4'].values
 
     # find individual alpha and snr
-    band, snr = individual_max_snr_band(x, FS)
+    band, snr = individual_max_snr_band(p4, FS)
     print(j_dataset, band, snr)
 
     # save x
-    an = band_hilbert(x, FS, band)[5 * FS:N_SAMPLES_TRAIN + N_SAMPLES_TEST+ 5 * FS]
-    x = x[5 * FS:N_SAMPLES_TRAIN + N_SAMPLES_TEST+ 5 * FS]
+    slc = slice(5 * FS, N_SAMPLES_TRAIN + N_SAMPLES_TEST+ 5 * FS)
+    an = band_hilbert(p4, FS, band)[slc]
+    x = x.values[slc]
+    p4 = p4[slc]
 
     # swap train test
     # an = np.concatenate([an[N_SAMPLES_TRAIN:N_SAMPLES_TRAIN + N_SAMPLES_TEST], an[:N_SAMPLES_TRAIN]])
     # x = np.concatenate([x[N_SAMPLES_TRAIN:N_SAMPLES_TRAIN + N_SAMPLES_TEST], x[:N_SAMPLES_TRAIN]])
 
     # train band
-    band_train, snr_train = individual_max_snr_band(x[:N_SAMPLES_TRAIN], FS)
+    band_train, snr_train = individual_max_snr_band(p4[:N_SAMPLES_TRAIN], FS)
     print(j_dataset, band_train, snr_train, '\n')
 
 
     # save info
-    band, snr = individual_max_snr_band(x[:N_SAMPLES_TRAIN], FS)
-    eeg_df = eeg_df.append(pd.DataFrame({'subj_id': dataset, 'snr': snr, 'band_left_train': band_train[0],
-                                         'band_right_train': band_train[1], 'band_left': band[0], 'band_right': band[1],
-                                         'eeg': x, 'an_signal': an},),
-                           ignore_index=True)
+    band, snr = individual_max_snr_band(p4, FS)
+    eeg_df_subj = pd.DataFrame(data=x, columns=rest_df.columns[:-1])
+    eeg_df_subj['subj_id'] = dataset
+    eeg_df_subj['snr'] = snr
+    eeg_df_subj['band_left_train'] = band_train[0]
+    eeg_df_subj['band_right_train'] = band_train[1]
+    eeg_df_subj['band_left'] = band[0]
+    eeg_df_subj['band_right'] = band[1]
+    eeg_df_subj['eeg'] = 'P4'
+    eeg_df_subj['an_signal'] = an
+    eeg_df = eeg_df.append(eeg_df_subj, ignore_index=True)
 
 
 # save train test data
-eeg_df.to_pickle('data/train_test_data.pkl')
+eeg_df.to_pickle('data/train_test_data_multichannel.pkl')
 
 
 import pylab as plt
-plt.plot(eeg_df['eeg'])
+plt.plot(eeg_df['P4'])

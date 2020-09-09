7574ce79791f1b51ff0feaf8124f4f98a9008ba5
nikolaims <n.m.smetanin@gmail.com>
2020-09-09 13:03:42 +0300

    colored noise

--- experiments/ukf_matsuda17_realdata_2bands.py
+++ experiments/ukf_matsuda17_realdata_2bands.py
@@ -11,7 +11,7 @@ from filters import CFIRBandEnvelopeDetector
 np.random.seed(42)
 
 eeg_df = pd.read_pickle('data/train_test_data.pkl')
-dataset = eeg_df.query('snr>1')['subj_id'].unique()[0]
+dataset = eeg_df.query('snr>0.9')['subj_id'].unique()[0]
 eeg_df = eeg_df.query('subj_id=={}'.format(dataset))
 
 delay=0
@@ -21,7 +21,7 @@ phase = np.angle(an_signal)
 
 eeg = eeg_df['eeg'].values*1e6
 band = eeg_df[['band_left', 'band_right']].values[0]
-cfir_envelope = np.abs(CFIRBandEnvelopeDetector(band, 500, delay).apply(eeg))
+cfir_envelope = 2*np.abs(CFIRBandEnvelopeDetector(band, 500, delay).apply(eeg))
 
 fs = 500
 f0 = np.mean(band)
@@ -47,10 +47,10 @@ F = block_diag(get_f(f0), get_f(1.5))
 
 H = np.array([1, 0, 1, 0])
 
-sigma = 0.1
+sigma = 0.12
 Q = np.eye(4) * sigma**2
 
-sigma_z = 1.5
+sigma_z = 2
 R = sigma_z**2
 
 
@@ -80,8 +80,14 @@ for t in range(1, n_steps):
 # plt.plot(np.abs(x_true_list[:, 0] + 1j*x_true_list[:, 1]))
 
 kf_env = np.abs(x_list[:, 0] + 1j*x_list[:, 1])
+kf_phase = np.angle(x_list[:, 0] + 1j*x_list[:, 1])
 plt.plot(kf_env, label='KF {:.3f}'.format(np.corrcoef(envelope[:n_steps], kf_env)[1, 0]))
 plt.plot(envelope[:n_steps], label='non-causal')
 plt.plot(cfir_envelope[:n_steps], label='cFIR {:.3f}'.format(np.corrcoef(envelope[:n_steps], cfir_envelope[:n_steps])[1, 0]))
 plt.legend()
+
+# plt.figure()
+# plt.plot(phase)
+# plt.plot(kf_phase)
+
 
